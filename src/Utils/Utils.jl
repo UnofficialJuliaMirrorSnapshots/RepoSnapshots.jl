8cbb891e680a419708f059e72d2fd4b25ff97cc5
Dilum Aluthge <dilum@aluthge.com>
2019-03-24 21:40:07 -0400

    Progress commit

---
+++ src/Utils/Utils.jl
@@ -0,0 +1,13 @@
+##### Beginning of file
+
+module Utils # Begin submodule OrganizationSnapshots.Utils
+
+__precompile__(true)
+
+include("git.jl")
+
+include("misc.jl")
+
+end # End submodule OrganizationSnapshots.Utils
+
+##### End of file
