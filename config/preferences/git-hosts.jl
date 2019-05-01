d616d9fd7a558a8c7cdd89bf37e1b18157b4a729
Dilum Aluthge <dilum@aluthge.com>
2019-05-01 18:34:45 -0400

    More useful/informative error messages

--- config/preferences/git-hosts.jl
+++ config/preferences/git-hosts.jl
@@ -1,6 +1,8 @@
 import Snapshots
 
-if ENV["GIT_HOST"] == "GITHUB"
+const GIT_HOST = lowercase(strip(get(ENV, "GIT_HOST", "",)))
+
+if GIT_HOST == "github"
     const GITHUB_SRC_ORGANIZATION = "UnofficialJuliaMirror"
     const GITHUB_DST_ORGANIZATION = "UnofficialJuliaMirrorSnapshots"
     const GITHUB_BOT_USERNAME = "UnofficialJuliaMirrorBot"
@@ -22,7 +24,7 @@ if ENV["GIT_HOST"] == "GITHUB"
             github_bot_personal_access_token =
                 GITHUB_BOT_PERSONAL_ACCESS_TOKEN,
             )
-elseif ENV["GIT_HOST"] == "GITLAB"
+elseif GIT_HOST == "gitlab"
     const GITLAB_SRC_GROUP = "UnofficialJuliaMirror"
     const GITLAB_DST_GROUP = "UnofficialJuliaMirrorSnapshots"
     const GITLAB_BOT_USERNAME = "UnofficialJuliaMirrorBot"
@@ -44,7 +46,7 @@ elseif ENV["GIT_HOST"] == "GITLAB"
             gitlab_bot_personal_access_token =
                 GITLAB_BOT_PERSONAL_ACCESS_TOKEN,
             )
-elseif ENV["GIT_HOST"] == "BITBUCKET"
+elseif GIT_HOST == "bitbucket"
     const BITBUCKET_SRC_TEAM = "UnofficialJuliaMirror"
     const BITBUCKET_DST_TEAM = "UnofficialJuliaMirrorSnapshots"
     const BITBUCKET_BOT_USERNAME = "UnofficialJuliaMirrorBot"
@@ -65,4 +67,11 @@ elseif ENV["GIT_HOST"] == "BITBUCKET"
             bitbucket_bot_app_password = BITBUCKET_BOT_APP_PASSWORD,
             )
 else
+    error(
+        string(
+            "You must set the \"GIT_HOST\" environment variable, ",
+            "and it must be set to one of the following values: ",
+            "GITHUB GITLAB BITBUCKET",
+            )
+        )
 end
